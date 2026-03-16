f9ac610400556a7d01cc2bbfb9655fb2737099c3
Daria <dasha.afanaseva.1206@mail.ru>
2026-03-16 18:23:30 +0300

    Step 6: variables and data types in JavaScript

---
+++ main.js
@@ -0,0 +1,59 @@
+// let age = 20;
+// let name = "Daria";
+// let isStudent = true;
+
+// console.log("Name ", name);
+// console.log("Age ", age);
+// console.log("Is student", isStudent);
+
+// let value = 10;
+// console.log(value);
+// value = "теперь текст";
+// console.log(value);
+// value = true;
+// console.log(value);
+
+
+// let userName = "Алексей";
+// console.log(`Привет ${userName}`);
+
+// let price = 99.99;
+// let tempe = -15;
+// let infin = 1 / 0;
+// let motAN = 0 /0;
+// console.log(0.1 +0.2);
+
+// let isAlive = true;
+// let Osd = age >= 18;
+
+// let x;
+// let y = undefined;
+
+//объекты
+// let person = {
+//     name : "Стас",
+//     age : 3,
+//     Student : false,
+//     sayHello : function( ) {
+//         console.log("привет");
+//     },
+// };
+// console.log(sayHello)
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log(sum(5,3))
+
+// let now = new Date();
+// console.log(now);
+
+// const num = [1,2,3];
+// num[0] = 10; 
+// console.log(num);
+
+const perdson = {name: "Daria" , age: 18};
+perdson.age = 19;
+perdson.citu = "Volgograd";
+console.log(perdson);
